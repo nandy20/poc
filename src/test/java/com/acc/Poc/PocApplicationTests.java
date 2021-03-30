5a35ca4af968ab0d6e073657bc66335d6505fd36
Nandhini R <v-nandhini.r@turvo.com>
2021-03-30 16:35:17 +0530

    initial commit

---
+++ src/test/java/com/acc/Poc/PocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acc.Poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
