13364ae2a9974528e69f6a96a045276a4d1463e1
Nandhini R <v-nandhini.r@turvo.com>
2021-03-30 16:53:52 +0530

    controller added

---
+++ Poc/src/main/java/com/acc/Poc/controller/CompositeController.java
@@ -0,0 +1,20 @@
+package com.acc.Poc.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.acc.Poc.dtos.salesforceObj;
+
+@RestController
+public class CompositeController {
+	
+	
+	@PostMapping("/sfObj")
+	public void executeComposite(@RequestBody salesforceObj obj ) {
+		
+		System.out.println(obj.getEntityName());
+		
+	}
+
+}
---
+++ Poc/src/main/java/com/acc/Poc/dtos/salesforceObj.java
@@ -0,0 +1,17 @@
+package com.acc.Poc.dtos;
+
+
+
+public class salesforceObj {
+
+	private String entityName;
+
+	public String getEntityName() {
+		return entityName;
+	}
+
+	public void setEntityName(String entityName) {
+		this.entityName = entityName;
+	}
+	
+}
