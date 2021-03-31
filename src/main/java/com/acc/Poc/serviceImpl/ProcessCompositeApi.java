64c2089bbb32a1cb5ce889fa1fce696331542fd0
Mr-Akk <akm99spl@gmail.com>
2021-03-31 12:26:34 +0530

    logic for processEachApi added

--- src/main/java/com/acc/Poc/serviceImpl/ProcessCompositeApi.java
+++ src/main/java/com/acc/Poc/serviceImpl/ProcessCompositeApi.java
@@ -5,6 +5,8 @@ import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -19,6 +21,9 @@ import com.google.gson.Gson;
 @Service
 public class ProcessCompositeApi {
 	
+	String username = "jayaram@accolitedigital.com";
+	String password = "Accolite@123Ljmq16LqC8ndmxau4fTKLhsQS";
+	
 	public void process(String filePath) {
 		
 		JSONArray js=jsonConverter(filePath);	
@@ -29,15 +34,12 @@ public class ProcessCompositeApi {
 		List<GenericApiRequest> list = new ArrayList<GenericApiRequest>();
 		for(Object obj : js) {
 			JSONObject jsonObject = (JSONObject)obj;
-			System.out.println(jsonObject);
-			
-			
+			JSONObject processedApi = processEachApi(jsonObject);
 			try {
 				Gson gson = new Gson();
-			     GenericApiRequest genericApi = gson.fromJson(jsonObject.toString(), GenericApiRequest.class);
+			     GenericApiRequest genericApi = gson.fromJson(processedApi.toString(), GenericApiRequest.class);
 			     list.add(genericApi);
 			} catch (Exception e) {
-
 				e.printStackTrace();
 			}
 		}
@@ -45,23 +47,34 @@ public class ProcessCompositeApi {
 	}
 	
 	private JSONArray jsonConverter(String filePath) {
-		
-		 
 		try {
 			JSONParser jsonParser = new JSONParser();
 		      FileReader reader = new FileReader(filePath);
 		      JSONObject configJson = (JSONObject) jsonParser.parse(reader);
 		      JSONArray requestListJson = (JSONArray) configJson.get("requestList");
 		      System.out.println(requestListJson);
-		      return requestListJson;
-			
+		      return requestListJson;		
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	     
-		
+
 		return null;
+	}
+	
+	private JSONObject processEachApi(JSONObject object) {
+		switch ((String)object.get("apiKey")) {
+		case "1":
+			JSONObject object2 = (JSONObject) object.get("apiRequest");
+			String object3 =(String) object2.get("requestBody");
+			String string = object3.replace("$USERNAME", username);
+			String string2 = string.replace("$PASSWORD", password);
+			object2.put("requestBody", string2);
+			return object2;
+		default:
+			break;
+		}
+		return object;
 	}
 
 }
